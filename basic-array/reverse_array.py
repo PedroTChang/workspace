69d7c8522c53b5d4a47bcd02d081a1209bf165c6
pedrotchang <pedrochang91@gmail.com>
2022-07-28 14:13:31 -0700

    commit

---
+++ basic-array/reverse_array.py
@@ -0,0 +1,7 @@
+# Given an array, write a function that returns an array that contains the original array’s values in reverse.
+# 
+# Input: [1, 2, 3, 4, 5]
+# Output: [5, 4, 3, 2, 1]
+
+numbers = [1, 2, 3, 4, 5]
+
