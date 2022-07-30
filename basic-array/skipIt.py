f09f4bab5a4e8b2ab629e48f4487fa799aecb313
pedrotchang <pedrochang91@gmail.com>
2022-07-29 18:00:09 -0700

    skipIt.py

---
+++ basic-array/skipIt.py
@@ -0,0 +1,17 @@
+# Given an array of numbers, write a function that returns a new array in which only select numbers from the original array are included, based on the following details:
+# 
+# The new array should always start with the first number from the original array. The next number that should be included depends on what the first number is. The first number dictates how many spaces to the right the computer should move to pick the next number. For example, if the first number is 2, then the next number that the computer should select would be two spaces to the right. This number gets added to the new array. If this next number happens to be a 4, then the next number after that is the one four spaces to the right. And so on and so forth until the end of the array is reached.
+# 
+# Input:
+# [2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2]
+# 
+# Output:
+# [2, 3, 1, 2, 2, 1, 5, 2, 2]
+
+
+
+numbers = [2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2]
+index = 0
+while (index < len(numbers)):
+    print(index)
+    index += 1
