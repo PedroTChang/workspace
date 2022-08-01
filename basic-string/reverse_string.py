3b1938e885fc108510b3582958a4eb16a52fe2f1
pedrotchang <pedrochang91@gmail.com>
2022-08-01 11:11:37 -0700

    reverse_string.py

---
+++ basic-string/reverse_string.py
@@ -0,0 +1,11 @@
+# Write a function that returns the reverse of a given string.
+
+def reverse(string):
+    result = ""
+    index = len(string) - 1
+    while index >= 0:
+        result += string[index]
+        index -= 1
+    return result
+
+print(reverse("abcde"))
