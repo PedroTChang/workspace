84d344c1a509d911b9ea819df5d4ab069fe67673
Pedro Chang <pedrotychang@gmail.com>
2022-10-12 12:29:22 -0700

    arraysMapPractice (while).js complete

---
+++ basic-array/mapDouble.js
@@ -0,0 +1,8 @@
+// Given an array of numbers, write a function that returns a new array whose values are the original array’s value doubled.
+
+// Input: [4, 2, 5, 99, -4]
+// Output: [8, 4, 10, 198, -8]
+
+function doubleIt(numbers) {
+  numbers.map 
+}
---
+++ skills_assesment/8. arraysMapPractice (while).js
@@ -0,0 +1,99 @@
+// 1. Start with an array of numbers and create a new array with each number times 3.
+//    For example, [1, 2, 3] becomes [3, 6, 9].
+// var numbers = [1, 2, 3];
+// var index = 0;
+// var timesThree = [];
+// while (index < numbers.length) {
+//   timesThree.push(numbers[index] * 3) 
+//   index += 1;
+// }
+// console.log(timesThree);
+
+// 2. Start with an array of strings and create a new array with each string upcased.
+//    For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+
+// 3. Start with an array of hashes and create a new array of string values from each hash's :name key.
+//    For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+
+// 4. Start with an array of numbers and create a new array with each number plus 7.
+//    For example, [1, 2, 3] becomes [8, 9, 10].
+// var numbers = [1, 2, 3]
+// var newNumbers = [];
+// var index = 0;
+// while (index < numbers.length) {
+//   newNumbers.push(numbers[index] + 7);
+//   index++
+// }
+// console.log(newNumbers)
+
+// 5. Start with an array of strings and create a new array with each string's length.
+//    For example, ["hello", "goodbye"] becomes [5, 7].
+// var strings = ["hello", "goodbye"];
+// var numbers = [];
+// index = 0;
+// while (index < strings.length) {
+//   numbers.push(strings[index].length)
+//   index++
+// }
+// console.log(numbers)
+
+// 6. Start with an array of hashes and create a new array of number values from each hash's :age key.
+//    For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
+// var people = [
+//   { name: "Alice", age: 27 },
+//   { name: "Blane", age: 16 }
+// ]
+// index = 0;
+// var age = [];
+// while (index < people.length) {
+//  age.push(people[index].age) 
+//  index++
+// }
+// console.log(age)
+
+// 7. Start with an array of numbers and create a new array with each number divided by 2.
+//    For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
+// var numbers = [1, 2, 3];
+// var index = 0;
+// var newNumbers = [];
+// while (index < numbers.length) {
+//   newNumbers.push(numbers[index]/2.0)
+//   index++
+// }
+// console.log(newNumbers)
+
+// 8. Start with an array of strings and create a new array with each string's first letter only.
+//    For example, ["hello", "goodbye"] becomes ["h", "g"].
+// var strings = ["I", "decree", "and", "declare:", "wealth", "is", "attracted", "to", "me!"]
+// var newStrings = [];
+// var index = 0;
+// while (index < strings.length) {
+//   newStrings.push(strings[index][0])
+//   index++
+// }
+// console.log(newStrings)
+
+//9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
+//    For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
+// var people = [
+//   { name: "Pedro", age: 30 },
+//   { name: "Hanna", age: 26 }
+// ]
+// var doubleAge = [];
+// index = 0;
+// while (index < people.length) {
+//   doubleAge.push(people[index].age * 2)
+//   index++
+// }
+// console.log(doubleAge)
+
+//10. Start with an array of numbers and create a new array with each number converted into a string.
+//    For example, [1, 2, 3] becomes ["1", "2", "3"].
+// var numbers = [1, 2, 3];
+// var strings = [];
+// var index = 0;
+// while (index < numbers.length) {
+//   strings.push(numbers[index].toString())
+//   index++
+// }
+// console.log(strings)
