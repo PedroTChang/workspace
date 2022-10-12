8bb46d8881b01459f16d4a5fb3f26077747bf4d4
Pedro Chang <pedrotychang@gmail.com>
2022-10-12 09:56:07 -0700

    completes the functions DP

--- skills_assesment/7. functionsPractice.js
+++ skills_assesment/7. functionsPractice.js
@@ -11,18 +11,51 @@
 // console.log(makeCapital("lower case"));
 
 //3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
-function 
+// function getDifference(number1, number2) {
+//   return number1 - number2;
+// }
+// console.log(getDifference(10, 9));
 
 //4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+// function square(number) {
+//   return number * number;
+// }
+// console.log(square(131312));
 
 //5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+// function returnFirst(string) {
+//   return string[0];
+// }
+// console.log(returnFirst("fireeeee"));
 
 //6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
+// function combineString(string1, string2, string3) {
+//   return string1 + string2 + string3;
+// }
+// console.log(combineString("w", "o", "w"));
 
 //7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+// function convertToString(number) {
+//   return number.toString();
+// }
+// console.log(convertToString(123));
 
 //8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+// function repeatString(string) {
+//   return string + string + string+ string+ string;
+// }
+// console.log(repeatString("wordd"));
 
 //9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
+// function average(number1, number2, number3) {
+//   var total = number1 + number2 + number3
+//   return total/3.0;
+// }
+// console.log(average(5, 10, 20));
 
 //10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+// function convertNumber(number) {
+//   number = (number * 10) + 30
+//   return number
+// }
+// console.log(convertNumber(10))
