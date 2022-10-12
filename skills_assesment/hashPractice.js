99cd558f72de81cc2e96764f58a1c720036d0492
Pedro Chang <pedrotychang@gmail.com>
2022-10-12 09:22:02 -0700

    hashPractice.js

---
+++ skills_assesment/hashPractice.js
@@ -0,0 +1,54 @@
+//1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines
+var person = { firstName: "Jeff", lastName: "Waters", email: "jeffwaters@test.com" }
+// console.log(person["firstName"])
+// console.log(person["lastName"])
+// console.log(person["email"])
+//? Is there a way we can loop through this?
+
+//2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+// var people = ["Jeff", "Sarah", "Angelica"];
+// console.log(people[0])
+
+//3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+// var menu = { "fries": 3, "soda": 2, "cheeseburger": 5 };
+// menu["ice cream"] = 4;
+// console.log(menu);
+
+//4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+// var book = { title: "The Alchemist", author: "Paulo Coelho", pages: 231, language: "Portuguese" }
+// console.log(book["title"])
+// console.log(book["author"])
+// console.log(book["pages"])
+// console.log(book["language"])
+
+//5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+// var books = [
+//   { title: "The Alchemist", author: "Paulo Coelho", pages: 231, language: "Portuguese" },
+//   { title: "The Brothers Karamazov", author: "Fyodor Dostoevsky", pages: 800, language: "Russian" }, 
+//   { title: "Market Maker's Edge", author: "Josh Lukeman", pages: 221, language: "English" }, 
+// ]
+// console.log(books[2].author);
+
+//6. Make a hash to store 3 different states and their capitals. Then add a new state and capital and print the hash to see the result.
+
+//7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+// var macBook = { brand: "Apple", model: "MacBook Pro", year: "2021" }
+// console.log(macBook["brand"])
+// console.log(macBook["model"])
+// console.log(macBook["year"])
+
+//8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+// var laptops = [
+//   { brand: "Apple", model: "MacBook Pro", year: "2021" }, 
+//   { brand: "System76", model: "Oryx Pro", year: "2023" }, 
+//   { brand: "Lenovo", model: "ThinkPad X1", year: "2022" },
+// ]
+// console.log(laptops[1]["model"])
+
+//9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+//10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+// var shirt = { brand: "Gucci", color: "Brown", size: "Large" }
+// console.log(shirt["brand"])
+// console.log(shirt["color"])
+// console.log(shirt.size)
