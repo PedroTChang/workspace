e539fe4606c58bb6d0b17ae965bf56ee0ce4bbc2
Pedro Chang <pedrotychang@gmail.com>
2022-10-21 10:28:29 -0700

    up to #5 filterpractice.js

--- skills_assesment/9SArraysSelectPracticeFilter.js
+++ skills_assesment/9SArraysSelectPracticeFilter.js
@@ -1,20 +1,36 @@
 // 1. Start with an array of numbers and create a new array with only the numbers less than 20.
 //    For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
-let numbers = [2, 32, 80, 18, 12, 3];
-let newNumbers = numbers.filter(number => number < 20);
-console.log(newNumbers);
+// let numbers = [2, 32, 80, 18, 12, 3];
+// let newNumbers = numbers.filter((number) => number < 20);
+// console.log(newNumbers);
 
 // 2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 //    For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+// let strings = ["winner", "winner", "chicken", "dinner"];
+// let wStrings = strings.filter((string) => string[0] === "w");
+// console.log(wStrings);
 
 // 3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 //    For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+// let items = [
+//   { name: "chair", price: 100 },
+//   { name: "pencil", price: 1 },
+//   { name: "book", price: 4 },
+// ];
+// let premiumItems = items.filter((item) => item.price > 5);
+// console.log(premiumItems);
 
 // 4. Start with an array of numbers and create a new array with only the even numbers.
 //    For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+// let numbers = [2, 4, 5, 1, 8, 9, 7];
+// let evens = numbers.filter((number) => number % 2 === 0);
+// console.log(evens)
 
 // 5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 //    For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+// let strings = ["a", "man", "a", "plan", "a", "canal", "panama"];
+// let aStrings = strings.filter((string) => string.length < 4);
+// console.log(aStrings);
 
 // 6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 //    For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
