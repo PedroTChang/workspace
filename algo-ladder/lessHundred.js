6035248b5aa8a3ccdf99ec194654dc03bc7a5401
Pedro Chang <pedrotychang@gmail.com>
2022-10-25 12:40:15 -0700

    algoladder exercises

---
+++ algo-ladder/lessHundred.js
@@ -0,0 +1,9 @@
+// Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
+
+// Input: [99, 101, 88, 4, 2000, 50]
+// Output: [99, 88, 4, 50]
+
+// function lessHundred(array) {
+//   return array.filter(number => number < 100);
+// }
+// console.log(lessHundred([99, 101, 88, 4, 2000, 50]));
---
+++ algo-ladder/reduceSum.js
@@ -0,0 +1,18 @@
+// Input: [1, 2, 3, 4]
+// Output: 10
+// Explanation: (1 + 2 + 3 + 4) = 10
+
+// const array = [1, 2, 3, 4];
+
+// const initial = 0;
+// const sum = array.reduce(
+//    (previous, current) => previous + current, initial 
+// );
+
+// console.log(sum)
+
+// 1
+// Write a function that accepts an array of numbers and returns an array with each number increased by 7.
+// const numbers = [1, 2, 3, 4, 5]
+// const newNumbers = numbers.map(number => number + 7);
+// console.log(newNumbers)
